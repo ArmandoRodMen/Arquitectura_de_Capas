b21d04ad7f6081a018b9d11ee3e9e88a004180d8
Armando Rodriguez <armandorodriguezm1998@gmail.com>
2023-12-13 16:18:47 -0600

    capas cookie

--- src/routes/cookie.router.js
+++ src/routes/cookie.router.js
@@ -1,6 +1,18 @@
 import { Router } from "express";
+import {
+    createCookie,
+    viewCookie,
+} from "../controllers/cookie.controller.js"
 
 const router = Router();
+
+//Session
+router.post("/", createCookie);
+router.get("/view", viewCookie);
+
+export default router;
+
+
 
 //Cookie
 /*
@@ -8,18 +20,3 @@ router.post("/", (req, res) =>{
     const {email} = req.body;
     res.cookie("user", email,{maxAge:10000}).send("Cookie created");
 });*/
-
-//Session
-router.post("/", (req, res) =>{
-    const {name, email} = req.body;
-    req.session.name= name;
-    req.session.email = email;
-    res.send("session");
-});
-
-router.get("/view", (req, res)=>{
-    console.log("view",req.cookies);
-    res.send("View cookies");
-});
-
-export default router;
